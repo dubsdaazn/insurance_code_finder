f3819b124713bc54f54481cb17afab863179983c
Edward Leung <EdwardLeung@localhost.(none)>
2012-11-19 23:08:38 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InsuranceCodeFinder::Application.config.secret_token = 'b3db370b8564c74aad86957a17de00b064b7d5e7a8b06607cb92cb2fc84172a06e7fe38e4340a9569c990d848120f7683f5c7a66f90e67aef9a4d26e4c0dc9e9'
