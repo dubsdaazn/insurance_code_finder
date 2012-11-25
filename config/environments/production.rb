a62d45172c07abeae7ca05725690c4c16f9b3e32
Edward Leung <EdwardLeung@localhost.(none)>
2012-11-25 00:07:02 -0500

    fixed css not compiling in heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ InsuranceCodeFinder::Application.configure do
 
   # Code is not reloaded between requests
   config.cache_classes = true
+  config.assets.precompile += %w( insurance_entries.css  )
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
